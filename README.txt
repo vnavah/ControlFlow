c7572c908f055323ea3630f5f0b06e02262dab3f
macuntu <vnavah.jobs@gmail.com>
2022-11-25 10:21:59 -0600

    up until "continue" statement

--- README.txt
+++ README.txt
@@ -1,7 +1,13 @@
 File Order
+blocks
+gessinggame
 conditions
 truefalse
 checkingin
 notin
 challenge
+forloops
 strings2
+ranges
+timestables
+shopping
--- challenge.py
+++ challenge.py
@@ -1,7 +1,11 @@
-name = input("Please tell us your name: ")
-age = int(input("Now your age "))
+# name = input("Please tell us your name: ")
+# age = int(input("Now your age "))
+#
+# if 18 <= age < 31:
+#     print("Welcome aboard {0}".format(name))
+# else:
+#     print("Sorry you can´t come on board")
 
-if 18 <= age < 31:
-    print("Welcome aboard {0}".format(name))
-else:
-    print("Sorry you can´t come on board")
+for i in range(0, 101):
+    if i % 7 == 0:
+        print(i)
