0910634614b439ab1d6c48928aa02d9647d5bfdf
macuntu <vnavah.jobs@gmail.com>
2022-11-26 12:20:09 -0600

    hilow game commit2

--- hilow.py
+++ hilow.py
@@ -4,13 +4,15 @@ high = 1000
 print("Please think of a number between {} and {}".format(low, high))
 input("Press ENTER to start...")
 
-guess = 1
+guesses = 1
 while True:
     guess = low + (high - low) // 2
     high_low = input("My guess is {}. Should I guess higher or lower? Enter h or l, or c if my guess was correct".format(guess)).casefold()
 
     if high_low == "h":
         # Guess higher. The low end of the range becomes 1 greater than the guess.
+        low = 500
+        guess = low + (high - low) / 2
 
     elif high_low == "l":
         # Guess lower. The high end of the range becomes 1 less than the guess.
